292fd1b078cd725c6470fb3510fb25a499273c08
Luz Gutierrez <mili01gm@gmail.com>
2017-03-09 18:06:09 -0500

    Ya salen los span de validacion

---
+++ js/validacion.js
@@ -0,0 +1,55 @@
+window.addEventListener('load',function(){
+
+  var cuentas = [{usuario: "mili01gm@gmail.com", password: "ArgosPeru"},
+                 {usuario: "alejvillaobos86@gmail.com", password: "mauriat1"},
+                 {usuario: "natalydv@gmail.com", password: "mundO2711"}];
+
+  var boton = document.getElementById('ingresa');
+  boton.addEventListener('click',function(){
+    var email = document.getElementById('user-mail');
+    var password = document.getElementById('user-contra');
+    var spanEmail = document.getElementById('msg-user');
+    var spanPass = document.getElementById('msg-contra');
+    var rightEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    function correctUser(param){
+      if(param == ""){
+        //alert("a");}
+        spanEmail.innerHTML = "El campo de usuario no puede estar en blanco"
+        return false;}
+      else if(!rightEmail.test(param)){
+        spanEmail.innerHTML = "Ingrese un usuario válido"
+        return false;}
+
+      //correctPassword(password.value);
+    }
+
+    function correctPassword(param){
+      if(param == ""){
+        spanPass.innerHTML = "El campo de password no puede estar en blanco"
+        return false;}
+      else if(param < 6){
+        spanPass.innerHTML = "Ingrese un password válido"
+        return false;}
+
+    }
+    // if(email.value ){
+    //   //alert("a");}
+    //   spanEmail.innerHTML = "El campo de usuario no puede estar en blanco"}
+    // else if(password.value.length == 0){
+    //   //alert("b");}
+    //   spanPass.innerHTML ="El campo de password no puede estar en blanco"}
+    //
+    // if(email.value.search(rightEmail)){
+    //    spanEmail.innerHTML = "Ingrese un email válido"}
+    // else if(password.value.search(rightPass){
+    //    spanPass.innerHTML = "Ingrese el password correcto"}
+
+
+    correctUser(email.value);
+    correctPassword(password.value);
+  });
+
+
+
+});
